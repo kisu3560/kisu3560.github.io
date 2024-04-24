0be433155302205c0dbd2bd6f5d640e4980d3cb9
kisu3560 <143726891+kisu3560@users.noreply.github.com>
2024-04-23 22:26:56 -0600

    Added random videos, took away random colors, made volume based on time watched

--- js/js.js
+++ js/js.js
@@ -1,55 +1,138 @@
 const body = document.querySelector("body");
 var buttonStart = document.querySelector("#buttonStart");
 buttonStart.addEventListener("click", startB);
-const paragraph = document.querySelector("p");
-console.log("YT Logic");
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-console.log("End YT logic");
+const volumeDisplay = document.getElementById("volume");
+const phraseDisplay = document.getElementById("phrase");
 
 let x = true;
-let start;
+let loaded = false;
+var video;
+var player;
+var playerElement;
+
+let videos = [
+    "W31e9meX9S4", // The Bee Movie but everytime they say Bee it gets faster
+    "o0u4M6vppCI", // Shia LaBeouf Live 
+    "wnQC6aoHnoQ", // Bart hits homer with a chair but it's Despacito
+    "5mGuCdlCcNM", // Bouncing DVD Logo 10 hours
+    "mHtq-mlzY7s", // Doofenshmirtz Evil Incorporated variations
+    "bZe5J8SVCYQ", // The Missile Knows Where It Is
+    "RXJKdh1KZ0w", // Rockwell Retro Encabulator
+    "Y82jDHRrswc", // The FitnessGram Pacer Test
+    "wFCIZS2iQ6w", // HEYAEYAEYYAYA but subbed in Chinese??
+    "MtN1YnoL46Q", // The Duck Song
+    "EIyixC9NsLI", // Badgers
+    "SaA_cs4WZHM", // The Kitty Cat Dance
+    "vXU8JwnO0Xk", // ASDFMovie 1-14
+    "tPEE9ZwTmy0", // The shortest video on youtube
+    "on0VlCWGSKY", // Peanut Butter Jelly Time 1 hour
+    "kpk2tdsPh0A", // Mario 64 half A-press
+    "4O9uOMy8BfY", // Peep injection video
+];
+
+let phrases = [
+    "The Bee Movie, what a classic! Could you understand what they were saying at the end?", // The Bee Movie but everytime they say Bee it gets faster
+    "Now run, it's Shia LaBeouf!", // Shia LaBeouf Live 
+    "Yeah, I don't know Spanish either.", // Bart hits homer with a chair but it's Despacito
+    "Did it hit the corner??", // Bouncing DVD Logo 10 hours
+    "'Cause he's Per-ry! Per-ry the Platypus!", // Doofenshmirtz Evil Incorporated variations
+    "The volume knows where it is because it also knows where it isn't.", // The Missile Knows Where It Is
+    "Using the finest pre-combinated technology, we can determine the ideal volume for perfect georesonant synchronicitation.", // Rockwell Retro Encabulator
+    "How was your workout?", // The FitnessGram Pacer Test
+    "Do you even know who He is?", // HEYAEYAEYYAYA but subbed in Chinese??
+    "No, I don't have any grapes - but I do have glue.", // The Duck Song
+    "Yes, that's a picture of a real badger.", // Badgers
+    "Meow", // The Kitty Cat Dance
+    "JKL;", // ASDFMovie 1-14
+    "Cat", // The shortest video on youtube
+    "For those allergic to Peanut Butter, I'm sorry.", // Peanut Butter Jelly Time 1 hour
+    "A percent is a percent. You can't say it's only a half.", // Mario 64 half A-press
+    "You just met my cat Peep! Ain't he a cutie?", // Peep injection video
+]
+
+let images = [
+    "", // The Bee Movie but everytime they say Bee it gets faster
+    "", // Shia LaBeouf Live 
+    "", // Bart hits homer with a chair but it's Despacito
+    "", // Bouncing DVD Logo 10 hours
+    "", // Doofenshmirtz Evil Incorporated variations
+    "", // The Missile Knows Where It Is
+    "", // Rockwell Retro Encabulator
+    "", // The FitnessGram Pacer Test
+    "", // HEYAEYAEYYAYA but subbed in Chinese??
+    "", // The Duck Song
+    "", // Badgers
+    "", // The Kitty Cat Dance
+    "", // ASDFMovie 1-14
+    "", // The shortest video on youtube
+    "", // Peanut Butter Jelly Time 1 hour
+    "", // Mario 64 half A-press
+    "", // Peep injection video
+]
+
+function pickVideo() {
+    video = Math.floor(Math.random()*videos.length);
+}
 
 function startB() {
     if(x) {
-        start = Date.now();
+        loadVideo();
+        if(playerElement) {
+            // Load and start video
+            playerElement.style.visibility = "visible"
+        }
+        if(player) {
+            pickVideo();
+            player.loadVideoById(videos[video]);
+            player.playVideo();
+        }
         buttonStart.textContent = "Stop";
         buttonStart.style.background = "red";
-        paragraph.textContent = "   ";
+        volumeDisplay.textContent = "   ";
+        phraseDisplay.textContent = "   ";
     } else {
-        let elapsed = Date.now() - start;
-        let volume = Math.floor(elapsed / (1000));
+        if(playerElement) {
+            playerElement.style.visibility = "hidden";
+        }
+        if(player) {
+            player.stopVideo();
+        }
+
+        let watchRatio = player.getCurrentTime() / player.getDuration();
+        console.log("Watched " + watchRatio + " = " + player.getCurrentTime() + "/" + player.getDuration());
+        let volume = Math.floor(watchRatio * 100);
         if(volume > 100) {
             volume = 100;
         }
 
-        body.style.background = "white";
-        if(volume < 50) {
-            paragraph.textContent = "Your volume is set to " + volume + "%. You want more? Wait longer next time!";
-        } else if(volume > 50) {
-            paragraph.textContent = "Your volume is set to " + volume + "%. Too loud? Too bad. You probably watched too long. Enjoy your seizure!";
-        } else {
-            paragraph.textContent = "Your volume is set to " + volume + "%. Congratulations! You got it to halfway! Who sets their volume to 50%?";
-        }
-
         buttonStart.style.background = "green";
         buttonStart.textContent = "restart";
+        volumeDisplay.textContent = "Your volume is " + volume;
+        phraseDisplay.textContent = phrases[video];
     }
 
     x = !x;
 }
-console.log("End fn");
-var player;
-function onYouTubeIframeAPIReady() {
+
+function loadVideo() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+window.onYouTubeIframeAPIReady = function() {
+    pickVideo();
+    console.log("Picked video " + video);
     console.log("API Ready");
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        videoId: 'M7lc1UVf-VE',
+        videoId: videos[video],
         playerVars: {
-          'playsinline': 1
+          'playsinline': 1,
+          'controls': 0,
+          'disablekb': 1
         },
         events: {
           'onReady': onPlayerReady,
@@ -58,22 +141,20 @@ function onYouTubeIframeAPIReady() {
       });
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     console.log("Player ready");
+    playerElement = document.getElementById("player");
     event.target.playVideo();
 }
 
-var done = false;
 function onPlayerStateChange(event) {
     console.log("Player state change: " + event.data);
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        done = true;
+    if (event.data == YT.PlayerState.PLAYING) {
+        //
+    } else if((event.data == YT.PlayerState.PAUSED) ||
+              (event.data == YT.PlayerState.ENDED)) {
     }
 }
-//randomation to change expetation like random numbers
+
 //go for annoying not tedious
 //white screeeen black text, boring
-//movie
-//% of time
-console.log("End script");
